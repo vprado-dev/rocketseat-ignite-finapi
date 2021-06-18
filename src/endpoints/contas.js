704302cf0524ccbe858c37fe78fb64a6c630c8c0
Vinicius Prado <vinicavagna@gmail.com>
2021-06-18 12:16:20 -0300

    :sparkles: added clientAlreadyExists handler

--- src/endpoints/contas.js
+++ src/endpoints/contas.js
@@ -1,16 +1,26 @@
 const { endpoint } = require("@ev-fns/endpoint");
 const { v4: uuidv4 } = require("uuid");
+const { HttpError } = require("@ev-fns/errors");
 
 const clientes = [];
 
-exports.contasPostOne = endpoint(async (req, res) => {
+exports.contasPostOne = endpoint((req, res) => {
   const { cpf, nome } = req.body;
+
+  const clienteAlreadyExists = clientes.some((cliente) => cliente.cpf === cpf);
+
+  if (clienteAlreadyExists) {
+    throw new HttpError(400, "Cliente already exists!");
+  }
 
   const id = uuidv4();
 
   clientes.push({
     cpf,
     nome,
-    id,
+    id: uuidv4(),
+    statement: [],
   });
+
+  res.status(201).end();
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { readdirSync } = require("fs");
 const { join } = require("path");
 const { json } = require("body-parser");
+const { notFound, exception } = require("@ev-fns/errors");
 
 const app = express();
 
@@ -12,6 +13,9 @@ const routes = readdirSync(join(__dirname, "routes"));
 for (const route of routes) {
   app.use(require(join(__dirname, "routes", route)));
 }
+
+app.use(notFound);
+app.use(exception);
 
 const PORT = 8000;
 
